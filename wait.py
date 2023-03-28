56bb06b3dc9de8c4c913cf6bdc8a01cf73b3bc64
hanchchch <cndghks15@gmail.com>
2023-03-28 11:46:51 +0000

    feat: watch syscall

--- wait.py
+++ wait.py
@@ -1,31 +1,66 @@
 import subprocess
-import ptrace.debugger
-import signal
-import sys
+from ptrace.debugger import (
+    PtraceDebugger,
+    ProcessExit,
+    ProcessSignal,
+    NewProcessEvent,
+    ProcessExecution,
+)
+from ptrace.func_call import FunctionCallOptions
+from ptrace.tools import signal_to_exitcode
 
 
 def debug(pid):
-    debugger = ptrace.debugger.PtraceDebugger()
+    debugger = PtraceDebugger()
 
     print("Attach the running process %s" % pid)
     process = debugger.addProcess(pid, False)
-    # process is a PtraceProcess instance
-    print("IP before: %#x" % process.getInstrPointer())
 
-    print("Execute a single step")
-    process.singleStep()
-    # singleStep() gives back control to the process. We have to wait
-    # until the process is trapped again to retrieve the control on the
-    # process.
-    process.waitSignals(signal.SIGTRAP)
-    print("IP after: %#x" % process.getInstrPointer())
+    process.syscall()
+    exitcode = 0
+    while True:
+        if not debugger:
+            break
+
+        try:
+            event = process.waitSyscall()
+        except ProcessExit as event:
+            print(event)
+            if event.exitcode is not None:
+                exitcode = event.exitcode
+            continue
+        except ProcessSignal as event:
+            print(event)
+            event.process.syscall(event.signum)
+            exitcode = signal_to_exitcode(event.signum)
+            continue
+        except NewProcessEvent as event:
+            continue
+        except ProcessExecution as event:
+            continue
+
+        syscall = process.syscall_state.event(
+            FunctionCallOptions(
+                write_types=False,
+                write_argname=False,
+                string_max_length=300,
+                replace_socketcall=True,
+                write_address=False,
+                max_array_count=20,
+            )
+        )
+        if syscall:
+            print(syscall.name)
+
+        process.syscall()
 
     process.detach()
     debugger.quit()
 
+    return exitcode
+
 
 def execute(commands: str) -> str:
-    """Run commands and return final output."""
     try:
         process = subprocess.Popen(
             commands,
@@ -42,4 +77,4 @@ def execute(commands: str) -> str:
     return output
 
 
-print(execute("echo hi"))
+print(execute("python3 -m http.server 8000"))

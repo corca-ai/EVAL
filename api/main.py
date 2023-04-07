c8ce069dddd0223368fa994f217e99fdeee585d0
hanchchch <cndghks15@gmail.com>
2023-04-07 19:48:58 +0900

    fix: files regex

--- api/main.py
+++ api/main.py
@@ -118,7 +118,8 @@ async def execute(request: ExecuteRequest) -> ExecuteResponse:
     except Exception as e:
         return {"answer": str(e), "files": []}
 
-    files = re.findall("[file/\S*]", res["output"])
+    files = re.findall(r"\[file/\S*\]", res["output"])
+    files = [file[1:-1] for file in files]
 
     return {
         "answer": res["output"],
